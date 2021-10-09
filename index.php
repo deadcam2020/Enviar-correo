4831bb906a692fbf5734191d47461e217575d870
deadcam2020 <85250604+deadcam2020@users.noreply.github.com>
2021-10-08 22:03:17 -0500

    Update and rename index.html to index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	
 	<h2>CONTACTO</h2>
 	<input type="text" name="nombre"  placeholder="Nombre"> <br><br>
-	<input type="text" name="correo"  placeholder="Correo"> <br><br>
+	<input type="email" name="correo"  placeholder="Correo"> <br><br>
 	<input type="text" name="telefono"  placeholder="Telefono"><br><br>
 	<textarea name="mensaje" placeholder="Escriba un mensaje"> 
 	</textarea><br><br>
